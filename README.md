ba912e17d28ca5bcd721f340938642b3623acfed
kikeestrada <kikeestrada@gmail.com>
2020-03-24 14:38:42 -0600

    First Load

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# myGulpBoilerplate Style guide
+## This is a Gulp BoilerPlate created by me for the test asigned for Wondersauce
 
+## For this Test i am using:
 
-https://kikeestrada.github.io/myStyleGuide/
+* Pug (HTML Template Engine)
+* Sass (CSS Preprocesor)
+* JS (es6 Babel Transpiler)
+* Markdown (For Agile Text)
+* json-data (For COnsuming Local Data)
+* gh-pages (For Continuous Deployment)
+
+##[For GitHub Pages Click Here](https://kikeestrada.github.io/wondersaucetest/)
+
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,5 @@
-import {topNav} from './modules/topNav'
-import {marvelApi} from './modules/marvelApi'
-import {searchFilter} from './modules/searchFilter'
-import {lightBox} from './modules/lightBox'
-import {tabs} from './modules/tabs'
-import {edModal} from './modules/modal'
-(()=>{
-	topNav();
-	lightBox();
-	tabs();
-	edModal();
-
-	if (document.body.classList.contains('home')) {
-		// functions here
-	}else if (document.body.classList.contains('page2')) {
-		// functions here
-		searchFilter();
-		marvelApi();
-	}else if (document.body.classList.contains('page3')) {
-		// functions here
-
-	}
-})();
+import {fnLoadApi} from "./modules/loadApi"
+import {hello,bye} from "./modules/example"
+hello();
+bye();
+fnLoadApi();
